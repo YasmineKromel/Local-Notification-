27da0e7cb266ca51eed5b370d0ba9116bd44938d
yasmine Soubhi kromel <yasmine@appstudio.pro>
2019-06-11 14:23:09 +0200

    Local notification commit 1 11 -6 - 2019

--- Local Notification/AppDelegate.swift
+++ Local Notification/AppDelegate.swift
@@ -7,18 +7,47 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+// inherit app delegate from UNUserNotificationCenterDelegate
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // delegate = self
+          UNUserNotificationCenter.current().delegate = self
         // Override point for customization after application launch.
+        
+        // this function to request from user a premission to get notification
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            print("granted: (\(granted)")
+        }
+              
         return true
     }
 
+    
+    // display notification when app in the foreground
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.badge ,.sound])
+    }
+    
+    
+     // handle notification when user taps 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        if response.notification.request.identifier == "TestIdentifier" {
+            print("handling notifications with the TestIdentifier Identifier")
+        }
+        
+        completionHandler()
+        
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -27,10 +56,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        print("application enter background ")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        print("apllication enter foregound ")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -43,4 +76,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
 
--- Local Notification/ViewController.swift
+++ Local Notification/ViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        createLocalNotification()
     }
+    
+    
+    func createLocalNotification()
+    {
+        // 1. define the content
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.body = "Body"
+        content.sound = UNNotificationSound.default
+        
+        //2. define trigger
+        //  UNCalendarNotificationTrigger : With this trigger you can specify date and time for the notification.
+        // UNLocationNotificationTrigger  : Here you can specify a location. When the user enters that region, the notification will be fired.
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        // 3.define request
+        let request = UNNotificationRequest(identifier: "TestIdentifier", content: content, trigger: trigger)
+        
+        // 4. add request
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+    }
+    
+ 
+    
+    
 
 
 }
